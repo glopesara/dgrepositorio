bae6e306f58d45dabe74a1bd94407ae0b2224eb9
Gabriel Araujo <gabriel.lopes.ara@gmail.com>
2020-11-09 12:07:39 -0300

    cadastro finalizado

--- assets/cadastro.js
+++ assets/cadastro.js
@@ -1,3 +1,109 @@
+function formulario_endereco(){
+    var cep = endereco.cep.value;
+    var rua = endereco.rua.value;
+    var numero = endereco.numero.value;
+    var cidade = endereco.cidade.value;
+    var bairro = endereco.bairro.value;
+    var uf = endereco.uf.value;
+     var verifica
+
+     if(cep.length != 9 ){
+         alert("Preencha o cep corretamente")
+     verifica = true
+ 
+     }
+ 
+    if(rua.length == 0){ 
+         alert("Digite o nome da rua");
+         verifica = true
+    }
+
+    if(numero.length == 0 ){ 
+     alert("Digite o numero da rua");
+     verifica = true
+     }
+
+     if(cidade.length == 0){ 
+        alert("Digite o nome da cidade");
+        verifica = true
+    }
+    if(bairro.length == 0){ 
+    alert("Digite o nome do bairro");
+    verifica = true
+    }
+    if(uf.length != 2){ 
+        alert("Digite a UF");
+        verifica = true
+        }
+    if(verifica){
+        return false;
+    } 
+    if(verifica != true){
+        alert("Endereço adicionado com sucesso")
+     } 
+ }
+
+function formulario_cartao(){
+    var namecard = fomulario.namecard.value;
+    var numbercard = fomulario.numbercard.value;
+    var cvv = fomulario.cvv.value;
+    var ano = fomulario.ano.value;
+     var verifica
+
+     if(namecard.indexOf(" ") == -1){
+         alert("Preencha o nome corretamente")
+     verifica = true
+ 
+     }
+ 
+    if(numbercard.length != 16 ){ 
+         alert("digite um valor de cartão valido");
+         verifica = true
+    }
+    if(cvv.length != 3 ){ 
+     alert("digite um CVV valido");
+     verifica = true
+     }
+     if(ano.length != 5 ){ 
+         alert("digite um ano valido");
+         verifica = true
+     }
+     if(verifica){
+         return false;
+     } 
+     if(verifica != true){
+         alert("Cartão adicionado com sucesso")
+     } 
+ }
+
+function mascara(sub, pg){
+    var i = pg.value.length;
+    var saida = sub.substring(0,1);
+    var alterar = sub.substring(i);
+    if (event.keyCode < 48 || event.keyCode > 57){
+      event.returnValue = false;
+    }
+    if (alterar.substring(0,1) != saida){
+              pg.value += alterar.substring(0,1);
+    }
+}
+function soletras(e){
+    key=e.keyCode || e.which;
+    teclado = String.fromCharCode(key).toLowerCase();
+    letras = "abcdefghijklmnopqrstuvwxyz "
+    especial = "8-37-38-46-164"
+    teclado_especial = false;
+    for (var i in especial) {
+        if(key==especial[i]){
+            teclado_especial=true;break;
+        }
+    }
+    if(letras.indexOf(teclado)==-1 && !teclado_especial){
+        return false
+    }
+}
+
+
 document.getElementById('dadosOff').addEventListener('click',e=>{
     document.getElementById("dadosCol").classList.add("close");
     document.getElementById('fomDados').classList.add('dadosOff');
@@ -37,64 +143,3 @@ document.getElementById('cartaoOn').addEventListener('click',e=>{
     document.getElementById("cartaoCol").classList.remove("close"); 
     document.getElementById('formCartao').classList.remove('pedidosOff');
 });
-
-function mascara(sub, pg){
-    var i = pg.value.length;
-    var saida = sub.substring(0,1);
-    var alterar = sub.substring(i);
-    if (event.keyCode < 48 || event.keyCode > 57){
-      event.returnValue = false;
-    }
-    if (alterar.substring(0,1) != saida){
-              pg.value += alterar.substring(0,1);
-    }
-}
-
-function soletras(e){
-    key=e.keyCode || e.which;
-    teclado = String.fromCharCode(key).toLowerCase();
-    letras = "abcdefghijklmnopqrstuvwxyz "
-    especial = "8-37-38-46-164"
-    teclado_especial = false;
-    for (var i in especial) {
-        if(key==especial[i]){
-            teclado_especial=true;break;
-        }
-    }
-    if(letras.indexOf(teclado)==-1 && !teclado_especial){
-        return false
-    }
-}
-
-function formulario_cartao(){
-    var namecard = fomulario.namecard.value;
-    var numbercard = fomulario.numbercard.value;
-    var cvv = fomulario.cvv.value;
-    var ano = fomulario.ano.value;
-     var verifica
-
-     if(namecard.indexOf(" ") == -1){
-         alert("Preencha o nome corretamente")
-     verifica = true
- 
-     }
- 
-    if(numbercard.length != 16 ){ 
-         alert("digite um valor de cartão valido");
-         verifica = true
-    }
-    if(cvv.length != 3 ){ 
-     alert("digite um CVV valido");
-     verifica = true
-     }
-     if(ano.length != 5 ){ 
-         alert("digite um ano valido");
-         verifica = true
-     }
-     if(verifica){
-         return false;
-     } 
-     if(verifica != true){
-         alert("PAGAMENTO REALIZADO COM SUCESSO")
-     } 
- }
