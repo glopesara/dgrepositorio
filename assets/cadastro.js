b3a8e4edc28935f02c4cc31a90fe6a1e17d7232c
Gabriel Araujo <gabriel.lopes.ara@gmail.com>
2020-11-09 10:33:22 -0300

    cadastro adicionando validações

--- assets/cadastro.js
+++ assets/cadastro.js
@@ -27,3 +27,74 @@ document.getElementById('pedidoOn').addEventListener('click',e=>{
     document.getElementById("pedidosCol").classList.remove("close"); 
     document.getElementById('formPedidos').classList.remove('pedidosOff');
 });
+
+document.getElementById('cartaoOff').addEventListener('click',e=>{
+    document.getElementById("cartaoCol").classList.add("close");
+    document.getElementById('formCartao').classList.add('pedidosOff');
+});
+
+document.getElementById('cartaoOn').addEventListener('click',e=>{
+    document.getElementById("cartaoCol").classList.remove("close"); 
+    document.getElementById('formCartao').classList.remove('pedidosOff');
+});
+
+function mascara(sub, pg){
+    var i = pg.value.length;
+    var saida = sub.substring(0,1);
+    var alterar = sub.substring(i);
+    if (event.keyCode < 48 || event.keyCode > 57){
+      event.returnValue = false;
+    }
+    if (alterar.substring(0,1) != saida){
+              pg.value += alterar.substring(0,1);
+    }
+}
+
+function soletras(e){
+    key=e.keyCode || e.which;
+    teclado = String.fromCharCode(key).toLowerCase();
+    letras = "abcdefghijklmnopqrstuvwxyz "
+    especial = "8-37-38-46-164"
+    teclado_especial = false;
+    for (var i in especial) {
+        if(key==especial[i]){
+            teclado_especial=true;break;
+        }
+    }
+    if(letras.indexOf(teclado)==-1 && !teclado_especial){
+        return false
+    }
+}
+
+function formulario_cartao(){
+    var namecard = fomulario.namecard.value;
+    var numbercard = fomulario.numbercard.value;
+    var cvv = fomulario.cvv.value;
+    var ano = fomulario.ano.value;
+     var verifica
+
+     if(namecard.indexOf(" ") == -1){
+         alert("Preencha o nome corretamente")
+     verifica = true
+ 
+     }
+ 
+    if(numbercard.length != 16 ){ 
+         alert("digite um valor de cartão valido");
+         verifica = true
+    }
+    if(cvv.length != 3 ){ 
+     alert("digite um CVV valido");
+     verifica = true
+     }
+     if(ano.length != 5 ){ 
+         alert("digite um ano valido");
+         verifica = true
+     }
+     if(verifica){
+         return false;
+     } 
+     if(verifica != true){
+         alert("PAGAMENTO REALIZADO COM SUCESSO")
+     } 
+ }
